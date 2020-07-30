ce64f80da5b7b006d76a07030cda18eb0c2a310c
undertakeout <68762017+undertakeout@users.noreply.github.com>
2020-07-30 12:18:00 -0400

    using tkinter to create mall GUI

--- MallLayout.py
+++ MallLayout.py
@@ -1,4 +1,23 @@
 # to do: make size of the mall map dependent on NameMall.Mall() characteristics
 #add stores
-import tkinter as tk
 
+import tkinter as tk
+root =tk.Tk()
+
+frame=tk.Frame(master=root,"sunken")
+greeting = tk.Label(master =frame, text="Generic Bootleg Mall Game",
+           fg = "green",
+           bg = "pink" )
+greeting.pack(fill = tk.BOTH)
+frame.pack(fill=tk.BOTH)
+
+startButton = tk.Button(text = 'Click to start',
+                        width = 15,
+                        height = 5,
+                        fg = "green",
+                        bg = "white")
+startButton.pack()
+
+
+
+root.mainloop()
